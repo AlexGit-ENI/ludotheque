7148247728e8aae50a8b73453296a49392cec035
AlexGit-ENI <alexis.joignant2026@campus-eni.fr>
2026-06-19 12:19:04 +0200

    Test ajout de jeu ok

--- src/main/java/fr/eni/ludotheque/bll/impl/JeuServiceImpl.java
+++ src/main/java/fr/eni/ludotheque/bll/impl/JeuServiceImpl.java
@@ -2,17 +2,24 @@ package fr.eni.ludotheque.bll.impl;
 
 import fr.eni.ludotheque.bll.JeuService;
 
+import fr.eni.ludotheque.bo.Jeu;
+import fr.eni.ludotheque.dal.JeuRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class JeuServiceImpl implements JeuService {
 
-//    private final JeuRepository jeuRepository;
-
+    public JeuServiceImpl(JeuRepository jeuRepository) {
+        this.jeuRepository = jeuRepository;
+    }
 
     @Override
-    public void ajouterUnGenre(JeuService jeuService) {
-
+    public Jeu ajouterJeu(Jeu jeu) {
+        return jeu;
     }
+
+    private final JeuRepository jeuRepository;
+
+//    private finale GenreRepository genreRepository;
 }
