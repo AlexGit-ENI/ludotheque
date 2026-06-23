9d35d6a5341dd4fdc890c3f717ced36cdff87224
Alexis <Alexis.joignant2026@campus-eni.fr>
2026-06-23 13:34:02 +0200

    JeuDTO Jeu service

--- src/main/java/fr/eni/ludotheque/bll/JeuService.java
+++ src/main/java/fr/eni/ludotheque/bll/JeuService.java
@@ -1,8 +1,15 @@
 package fr.eni.ludotheque.bll;
 
 import fr.eni.ludotheque.bo.Jeu;
+import fr.eni.ludotheque.dto.JeuDisponibleDTO;
+
+import java.util.List;
 
 public interface JeuService {
 
     Jeu ajouterJeu (Jeu jeu);
+
+    List<Jeu> FindAll ();
+
+    List<JeuDisponibleDTO> getDisponibilite();
 }
