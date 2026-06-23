60148e364bd8a171fba5f5a74ef25bfb988f6cf2
Alexis <Alexis.joignant2026@campus-eni.fr>
2026-06-23 15:22:35 +0200

    LocationServiceImpl

--- src/main/java/fr/eni/ludotheque/bll/impl/LocationServiceImpl.java
+++ src/main/java/fr/eni/ludotheque/bll/impl/LocationServiceImpl.java
@@ -1,8 +1,12 @@
 package fr.eni.ludotheque.bll.impl;
 import fr.eni.ludotheque.bll.LocationService;
+import fr.eni.ludotheque.bo.Facture;
 import fr.eni.ludotheque.bo.Location;
 import fr.eni.ludotheque.dal.LocationRepository;
+import fr.eni.ludotheque.dto.LocationDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LocationServiceImpl implements LocationService {
@@ -15,6 +19,26 @@ public class LocationServiceImpl implements LocationService {
     @Override
     public Location exemplaireLoue(Location location) {
         return locationRepository.save(location);
+    }
+
+    @Override
+    public Location ajouterLocation(LocationDTO locationDto) {
+        return null;
+    }
+
+    @Override
+    public Facture retourExemplaires(List<String> codebarres) {
+        return null;
+    }
+
+    @Override
+    public Facture payerFacture(Integer noFacture) {
+        return null;
+    }
+
+    @Override
+    public void trouverLocationParExemplaireCodebarre(String codebarre) {
+
     }
 }
 
